5613b4e1844d3dbac1836748e05740612b747c3d
Joud Khattab <khattab.joud@gmail.com>
2019-01-26 18:14:29 +0200

    Add files via upload

---
+++ Lecture 2 (Framework)/CG Lesson2/CG Lesson2/GLClass.cs
@@ -0,0 +1,33 @@
+﻿using CsGL.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_Lesson2
+{
+    public class GLClass : OpenGLControl
+    {
+
+        protected override void InitGLContext()
+        {
+            base.InitGLContext();
+        }
+
+        public override void glDraw()
+        {
+            base.glDraw();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+
+        }
+
+
+    }
+
+
+}
