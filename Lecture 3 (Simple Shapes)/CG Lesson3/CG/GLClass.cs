136c8cc7e15ef4444a545a13caf36db38b5d804b
Joud Khattab <khattab.joud@gmail.com>
2019-01-26 18:20:34 +0200

    Add files via upload

---
+++ Lecture 3 (Simple Shapes)/CG Lesson3/CG/GLClass.cs
@@ -0,0 +1,108 @@
+﻿using CsGL.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG
+{
+    public class GLClass : OpenGLControl
+    {
+
+        protected override void InitGLContext()
+        {
+            base.InitGLContext();
+
+
+        }
+
+        public override void glDraw()
+        {
+            base.glDraw();
+            //drawTriangle();
+            //drawPoints();
+            //drawSquare();
+            //drawLines();
+            drawTwoShapes();
+        }
+
+        void drawTriangle()
+        {
+            GL.glBegin(GL.GL_TRIANGLES);
+            GL.glColor3d(1, 0, 0);
+            GL.glVertex3d(-1, -1, -1);
+            GL.glColor3d(0, 1, 0);
+            GL.glVertex3d(1, -1, -1);
+            GL.glColor3d(0, 0, 1);
+            GL.glVertex3d(0, 1, -1);
+            GL.glEnd();
+        }
+
+        void drawSquare()
+        {
+            GL.glBegin(GL.GL_POLYGON);
+            GL.glColor3d(1, 1, 0);
+            GL.glVertex3d(0.5, 0.5, 0);
+            GL.glVertex3d(-0.5, 0.5, 0);
+            GL.glVertex3d(-0.5, -0.5, 0);
+            GL.glVertex3d(0.5, -0.5, 0);
+            GL.glEnd();
+        }
+
+        void drawLines()
+        {
+            GL.glBegin(GL.GL_LINES);
+            GL.glColor3d(1, 1, 0);
+            GL.glVertex3d(0.5, 0.5, 0);
+            GL.glVertex3d(-0.5, 0.5, 0);
+            GL.glVertex3d(-0.5, -0.5, 0);
+            GL.glVertex3d(0.5, -0.5, 0);
+            GL.glEnd();
+        }
+
+        void drawPoints()
+        {
+            GL.glPointSize(50);
+            GL.glBegin(GL.GL_POINTS);
+            GL.glColor3d(1, 0, 0);
+            GL.glVertex3d(-1, -1, -1);
+            GL.glColor3d(0, 1, 0);
+            GL.glVertex3d(1, -1, -1);
+            GL.glColor3d(0, 0, 1);
+            GL.glVertex3d(0, 1, -1);
+            GL.glEnd();
+        }
+
+        private void drawTwoShapes()
+        {
+            GL.glBegin(GL.GL_TRIANGLES);
+            GL.glColor3d(1, 0, 0);
+            GL.glVertex3f(-0.7f, 0f, 0.0f);
+            GL.glColor3d(0, 1, 0);
+            GL.glVertex3f(-0.3f, 0f, 0.0f);
+            GL.glColor3d(0, 0, 1);
+            GL.glVertex3f(-0.5f, 0.5f, 0.0f);
+            GL.glEnd();
+
+
+            GL.glBegin(GL.GL_POLYGON);
+            GL.glColor3f(0, 0.7f, 0.70f);
+            GL.glVertex3f(0.3f, 0f, 0.0f);
+            GL.glVertex3f(0.7f, 0f, 0.0f);
+            GL.glVertex3f(0.7f, 0.5f, 0.0f);
+            GL.glVertex3f(0.3f, 0.5f, 0.0f);
+            GL.glEnd();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+
+        }
+
+
+    }
+
+
+}
---
+++ Lecture 4 (Circle)/CG Lesson4/CG/GLClass.cs
@@ -0,0 +1,87 @@
+﻿using CsGL.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG
+{
+    public class GLClass : OpenGLControl
+    {
+
+        protected override void InitGLContext()
+        {
+            base.InitGLContext();
+
+
+        }
+
+        public override void glDraw()
+        {
+            base.glDraw();
+            //drawLines();
+            //drawTwoShapes();
+            drawCircle();
+        }
+
+        void drawTwoTriangle()
+        {
+            // GL.glBegin(GL.GL_TRIANGLES);
+            //GL.glBegin(GL.GL_TRIANGLE_STRIP);
+            GL.glBegin(GL.GL_TRIANGLE_FAN);
+            GL.glColor3d(1, 0, 0);
+            GL.glVertex3d(-0.3, -0.2, 0);
+            GL.glVertex3d(0.1, -0.2, 0);
+            GL.glVertex3d(-0.1, 0.2, 0);
+
+            GL.glColor3d(0, 1, 0);
+            GL.glVertex3d(0.1, 0, 0);
+            GL.glVertex3d(0.5, 0, 0);
+            GL.glVertex3d(0.3, 0.4, 0);
+            GL.glEnd();
+        }
+
+        void drawCircle()
+        {
+            GL.glBegin(GL.GL_TRIANGLE_FAN);
+            
+            // Center Point
+            ////////////
+            GL.glVertex3d(0, 0, 0);
+            ////////////
+            double R = 0.5;
+            for(double O=0;O<=2*Math.PI;O=O+0.005)
+            {
+                // x2 + y2 = r2
+                double y = Math.Sin(O) * R;
+                double x = Math.Cos(O) * R;
+                GL.glColor3d(x, y, 0);
+                GL.glVertex3d(x, y, 0);
+            }
+            GL.glEnd();
+        }
+
+        void drawLines()
+        {
+            GL.glBegin(GL.GL_LINE_STRIP);
+            GL.glColor3d(1, 1, 0);
+            GL.glVertex3d(0.5, 0.5, 0);
+            GL.glVertex3d(-0.5, 0.5, 0);
+            GL.glVertex3d(-0.5, -0.5, 0);
+            GL.glVertex3d(0.5, -0.5, 0);
+            GL.glVertex3d(0.5, 0.5, 0);
+            GL.glEnd();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+
+        }
+
+
+    }
+
+
+}
